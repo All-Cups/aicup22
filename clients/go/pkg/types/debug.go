3101f271ba07a97d7fbfe083bbe33e58116e0190
SVI <prospero.78.su@gmail.com>
2022-07-09 22:54:38 +0300

    Исправление импорта, стилизация, упрощение

--- clients/go/pkg/types/debug.go
+++ clients/go/pkg/types/debug.go
@@ -5,7 +5,7 @@ import (
 
 	"aicup22/pkg/codegame"
 	"aicup22/pkg/debugging"
-	. "aicup22/pkg/model"
+	"aicup22/pkg/model"
 )
 
 type DebugInterface struct {
@@ -13,35 +13,42 @@ type DebugInterface struct {
 	Writer *bufio.Writer
 }
 
-func (debugInterface DebugInterface) AddPlacedText(position Vec2, text string, alignment Vec2, size float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddPlacedText(position model.Vec2, text string, alignment model.Vec2, size float64, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataPlacedText(position, text, alignment, size, color))
 }
 
-func (debugInterface DebugInterface) AddCircle(position Vec2, radius float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddCircle(position model.Vec2, radius float64, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataCircle(position, radius, color))
 }
 
-func (debugInterface DebugInterface) AddGradientCircle(position Vec2, radius float64, innerColor debugging.Color, outerColor debugging.Color) {
+func (debugInterface DebugInterface) AddGradientCircle(position model.Vec2, radius float64,
+	innerColor, outerColor debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataGradientCircle(position, radius, innerColor, outerColor))
 }
 
-func (debugInterface DebugInterface) AddRing(position Vec2, radius float64, width float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddRing(position model.Vec2,
+	radius, width float64,
+	color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataRing(position, radius, width, color))
 }
 
-func (debugInterface DebugInterface) AddPie(position Vec2, radius float64, startAngle float64, endAngle float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddPie(position model.Vec2,
+	radius, startAngle, endAngle float64,
+	color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataPie(position, radius, startAngle, endAngle, color))
 }
 
-func (debugInterface DebugInterface) AddArc(position Vec2, radius float64, width float64, startAngle float64, endAngle float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddArc(position model.Vec2,
+	radius, width, startAngle, endAngle float64,
+	color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataArc(position, radius, width, startAngle, endAngle, color))
 }
 
-func (debugInterface DebugInterface) AddRect(bottomLeft Vec2, size Vec2, color debugging.Color) {
+func (debugInterface DebugInterface) AddRect(bottomLeft, size model.Vec2, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataRect(bottomLeft, size, color))
 }
 
-func (debugInterface DebugInterface) AddPolygon(vertices []Vec2, color debugging.Color) {
+func (debugInterface DebugInterface) AddPolygon(vertices []model.Vec2, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataPolygon(vertices, color))
 }
 
@@ -49,15 +56,15 @@ func (debugInterface DebugInterface) AddGradientPolygon(vertices []debugging.Col
 	debugInterface.Add(debugging.NewDebugDataGradientPolygon(vertices))
 }
 
-func (debugInterface DebugInterface) AddSegment(firstEnd Vec2, secondEnd Vec2, width float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddSegment(firstEnd, secondEnd model.Vec2, width float64, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataSegment(firstEnd, secondEnd, width, color))
 }
 
-func (debugInterface DebugInterface) AddGradientSegment(firstEnd Vec2, firstColor debugging.Color, secondEnd Vec2, secondColor debugging.Color, width float64) {
+func (debugInterface DebugInterface) AddGradientSegment(firstEnd model.Vec2, firstColor debugging.Color, secondEnd model.Vec2, secondColor debugging.Color, width float64) {
 	debugInterface.Add(debugging.NewDebugDataGradientSegment(firstEnd, firstColor, secondEnd, secondColor, width))
 }
 
-func (debugInterface DebugInterface) AddPolyLine(vertices []Vec2, width float64, color debugging.Color) {
+func (debugInterface DebugInterface) AddPolyLine(vertices []model.Vec2, width float64, color debugging.Color) {
 	debugInterface.Add(debugging.NewDebugDataPolyLine(vertices, width, color))
 }
 
